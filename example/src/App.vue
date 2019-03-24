1cb3c570eb1712965c56104599d9abceaa80dfc2
Дмитрий <dmitry.potapov.98@gmail.com>
2019-03-24 19:51:53 +0000

    null

--- example/src/App.vue
+++ example/src/App.vue
@@ -3,10 +3,10 @@
   <div class="row">
     <div class="col-9">
       <h5>The graph does not use third-party libraries. All code is written from scratch.</h5>
-      <!-- <Chart :json="json[0]" ref="chart0" id="chart0" :locale="locale" :theme="theme" />
+      <Chart :json="json[0]" ref="chart0" id="chart0" :locale="locale" :theme="theme" />
       <Chart :json="json[1]" ref="chart1" id="chart1" :locale="locale" :theme="theme" />
-      <Chart :json="json[2]" ref="chart2" id="chart2" :locale="locale" :theme="theme" /> -->
-      <!-- <Chart :json="json[3]" ref="chart3" id="chart3" :locale="locale" :theme="theme" /> -->
+      <Chart :json="json[2]" ref="chart2" id="chart2" :locale="locale" :theme="theme" />
+      <Chart :json="json[3]" ref="chart3" id="chart3" :locale="locale" :theme="theme" />
       <Chart :json="json[4]" ref="chart4" id="chart4" :locale="locale" :theme="theme" />
     </div>
 
